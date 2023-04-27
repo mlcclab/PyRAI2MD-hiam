62e3b24d8159a3cfb8064f8de887a1a0baf3af6c
Jingbai Li <lijingbai2009@gmail.com>
2023-04-27 12:20:56 +0800

    update charge embedding

--- PyRAI2MD/Utils/coordinates.py
+++ PyRAI2MD/Utils/coordinates.py
@@ -120,7 +120,7 @@ def read_coord(mol):
 def read_charge(mol):
     ## This function read point charges from files
     if not os.path.exists('%s.charge' % mol):
-        return np.zeros(0)
+        sys.exit('\n  FileNotFoundError\n  PyRAI2MD: looking for charge file %s.charge' % key)
 
     with open('%s.charge' % mol) as charge_file:
         file = charge_file.read().splitlines()
